d6d73edd4beb36010fdeede6b68d08b3b277b47a
Tapendra Shahi <tapendrashahi61@gmail.com>
2026-02-16 04:30:18 +0545

    Camera abstraction

---
+++ include/renderer/camera.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "glm/glm.hpp"
+
+enum class CameraMode {
+	PERSPECTIVE,
+	ORTHOGRAPHIC
+};
+
+class Camera {
+	private:
+		glm::vec3 camera_position_;
+		glm::vec3 camera_target_;
+		float radius_ = 4.0f;
+		CameraMode camera_mode_ = CameraMode::PERSPECTIVE;
+		float aspect_ratio_;
+		float field_of_view_;
+		float near_plane_;
+		float far_plane_;
+		int screen_width_;
+		int screen_height_;
+		
+		const glm::vec3 world_up_ {0.0f, 1.0f, 0.0f};
+		glm::vec3 camera_forward_ = glm::vec3(0.0f, 0.0f, 0.0f);
+		
+		float yaw_ = 0.0f;
+		float pitch_ = 0.0f;
+		
+	public:
+		Camera(int screen_width, int screen_height, glm::vec3 camera_position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 camera_target = glm::vec3(0.0f, 0.0f, 0.0f), float field_of_view = 45.0f, float near_plane = 0.1f, float far_plane = 100.0f);
+		
+		void set_camera_mode(CameraMode new_mode) { camera_mode_ = new_mode; }
+		void set_camera_target(const glm::vec3& new_target) { camera_target_ = new_target; }
+		void update_aspect_ratio(const int new_screen_width, const int new_screen_height);
+		
+		glm::mat4 get_view_matrix() const;
+		glm::mat4 get_projection_matrix() const;
+		
+		void update_camera_vectors();
+};
---
+++ src/renderer/camera.cpp
@@ -0,0 +1,43 @@
+#include "renderer/camera.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+Camera::Camera(int screen_width, int screen_height, glm::vec3 camera_position, glm::vec3 camera_target, float field_of_view, float near_plane, float far_plane) :
+	camera_position_(camera_position),
+	camera_target_(camera_target),
+	field_of_view_(field_of_view),
+	near_plane_(near_plane),
+	far_plane_(far_plane),
+	screen_width_(screen_width),
+	screen_height_(screen_height) {
+	aspect_ratio_ = static_cast<float>(screen_width_) / static_cast<float>(screen_height_);
+	
+	update_camera_vectors();
+}
+
+void Camera::update_aspect_ratio(const int new_screen_width, const int new_screen_height) {
+	aspect_ratio_ = static_cast<float>(new_screen_width) / static_cast<float>(new_screen_height);
+}
+
+glm::mat4 Camera::get_view_matrix() const {
+	return glm::lookAt(camera_position_, camera_target_, world_up_);
+}
+
+glm::mat4 Camera::get_projection_matrix() const {
+	if(camera_mode_ == CameraMode::ORTHOGRAPHIC) {
+		return glm::ortho(0.0f, static_cast<float>(screen_width_), 0.0f, static_cast<float>(screen_height_), near_plane_, far_plane_);
+	}
+	
+	return glm::perspective(glm::radians(field_of_view_), aspect_ratio_, near_plane_, far_plane_);
+}
+
+void Camera::update_camera_vectors() {
+	glm::vec3 target(0.0f);
+	target.x = radius_ * cos(glm::radians(yaw_)) * cos(glm::radians(pitch_));
+	target.y = radius_ * sin(glm::radians(pitch_));
+	target.z = radius_ * sin(glm::radians(yaw_)) * cos(glm::radians(pitch_));
+	
+	camera_position_ = target + camera_target_;
+	camera_forward_ = glm::normalize(camera_target_ - camera_position_);
+}
