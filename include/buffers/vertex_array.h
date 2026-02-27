00335138422c56f8ebccb42677d58eb32d6974dd
Tapendra Shahi <tapendrashahi61@gmail.com>
2026-02-27 08:38:41 +0545

    VertexArray header abstraction

---
+++ include/buffers/vertex_array.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <vector>
+#include <cstdint>
+
+#include "buffers/vertex_buffer.h"
+#include "buffers/index_buffer.h"
+
+class VertexArray {
+	private:
+		unsigned int vertex_array_id_ = 0;
+		std::vector<const VertexBuffer*> vertex_buffers_;
+		const IndexBuffer* index_buffer_ = nullptr;
+		uint32_t attribute_index_counter_ = 0;
+		
+	public:
+		VertexArray();
+		VertexArray(const VertexArray&)=delete;
+		VertexArray& operator=(const VertexArray&)=delete;
+		VertexArray(VertexArray&& other) noexcept;
+		VertexArray& operator=(VertexArray&& other) noexcept;
+		
+		void bind() const;
+		void unbind() const;
+		
+		void add_vertex_buffer(const VertexBuffer& vbo);
+		void set_index_buffer(const IndexBuffer& ibo);
+};
