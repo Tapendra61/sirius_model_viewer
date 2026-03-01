6aba46d539ba2c0b47ff71d58e347dbadc3ebc98
Tapendra Shahi <tapendrashahi61@gmail.com>
2026-03-01 17:22:21 +0545

    Impl model process mesh

--- src/model/model.cpp
+++ src/model/model.cpp
@@ -1,10 +1,13 @@
 #include "model/model.h"
 
 #include <string>
+#include <vector>
+#include <cstdint>
 
 #include "assimp/Importer.hpp"
 #include "assimp/scene.h"
 #include "assimp/postprocess.h"
+#include "glm/glm.hpp"
 
 #include "sirius_logger/log.h"
 #include "model/mesh.h"
@@ -37,5 +40,45 @@ void Model::process_node(aiNode* node, const aiScene* scene) {
 }
 
 Mesh Model::process_mesh(aiMesh* mesh, const aiScene* scene) {
+	std::vector<Vertex> vertices;
+	std::vector<uint32_t> indices;
 	
+	for(int i = 0; i < mesh->mNumVertices; i++) {
+		Vertex vertex{};
+		
+		vertex.position = glm::vec3(
+			mesh->mVertices[i].x,
+			mesh->mVertices[i].y,
+			mesh->mVertices[i].z			
+		);
+		
+		if(mesh->HasNormals()) {
+			vertex.normal = glm::vec3(
+				mesh->mNormals[i].x,
+				mesh->mNormals[i].y,
+				mesh->mNormals[i].z
+			);
+		}
+		
+		if(mesh->mTextureCoords[0]) {
+			vertex.tex_coords = glm::vec2(
+				mesh->mTextureCoords[0][i].x,
+				mesh->mTextureCoords[0][i].y
+			);
+		}
+		else {
+			vertex.tex_coords = glm::vec2(0.0f);
+		}
+		
+		vertices.push_back(vertex);
+	}
+	
+	for(unsigned int i = 0; i < mesh->mNumFaces; i++) {
+		aiFace face = mesh->mFaces[i];
+		for(unsigned int j = 0; j < face.mNumIndices; j++) {
+			indices.push_back(face.mIndices[j]);
+		}
+	}
+	
+	return Mesh(vertices, indices);
 }
